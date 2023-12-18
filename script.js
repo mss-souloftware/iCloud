6480cb6afcb093ce85e23bf1a96ef526b0acf9b9
mss-souloftware <sufyan.souloftware@gmail.com>
2023-12-18 18:29:26 +0500

    desktop complete

--- script.js
+++ script.js
@@ -2,4 +2,18 @@
     $('.userProfile').click(function () {
         $('#profilePop').toggleClass('show');
     })
+
+    $('.toggler').click(function () {
+        $('#lists').toggleClass('hide');
+        $('#items').toggleClass('hide');
+        $('#details').toggleClass('widthFull');
+    })
+
+    $("#lists, #items").resizable({
+        handles: "e",
+        resize: function (event, ui) {
+            console.log("Resized", ui.size.width, ui.size.height);
+        }
+    });
+
 }(jQuery))
