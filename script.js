d6faf7cec10a52c850a93e50bb7e69cc416f5bfd
mss-souloftware <sufyan.souloftware@gmail.com>
2023-12-21 18:25:19 +0500

    responsive complete

--- script.js
+++ script.js
@@ -3,17 +3,39 @@
         $('#profilePop').toggleClass('show');
     })
 
+    let screenWidth = $(window).width();
+
+    if (screenWidth <= 991) {
+        $('#itemLists li').click(function () {
+            $('#items').addClass('active');
+        })
+
+        $('#backInbox').click(function () {
+            $('#items').removeClass('active');
+        });
+
+        $('#productPanel .productCard').click(function () {
+            $('#details').addClass('active');
+        })
+
+        $('#backItem').click(function () {
+            $('#details').removeClass('active');
+        });
+    }
+
+
     $('.toggler').click(function () {
         $('#lists').toggleClass('hide');
         $('#items').toggleClass('hide');
         $('#details').toggleClass('widthFull');
     })
 
-    $("#lists, #items").resizable({
-        handles: "e",
-        resize: function (event, ui) {
-            console.log("Resized", ui.size.width, ui.size.height);
-        }
-    });
-
+    if (screenWidth >= 991) {
+        $("#lists, #items").resizable({
+            handles: "e",
+            resize: function (event, ui) {
+                console.log("Resized", ui.size.width, ui.size.height);
+            }
+        });
+    }
 }(jQuery))
