28d00734a57ae4316e10bfdddc1e793ca31ed56f
mss-souloftware <sufyan.souloftware@gmail.com>
2023-12-29 14:04:36 +0500

    feedback 1 complete

--- script.js
+++ script.js
@@ -3,39 +3,68 @@
         $('#profilePop').toggleClass('show');
     })
 
-    let screenWidth = $(window).width();
+    $('#search, #cancel').click(function () {
+        $('#search').toggleClass('show');
+        $('#searchbar').toggleClass('show');
+        $('#items h2').toggleClass('show');
 
-    if (screenWidth <= 991) {
-        $('#itemLists li').click(function () {
-            $('#items').addClass('active');
-        })
+        if ($('#items h2').hasClass('show')) {
+            $('#items h2').text('Search Items');
+        } else {
+            $('#items h2').text('Items');
+        }
+    })
 
-        $('#backInbox').click(function () {
-            $('#items').removeClass('active');
-        });
+    function adjustLayout() {
+        let screenWidth = $(window).width();
 
-        $('#productPanel .productCard').click(function () {
-            $('#details').addClass('active');
-        })
+        if (screenWidth <= 991) {
+            $('#lists,#items,#details').removeAttr('class');
 
-        $('#backItem').click(function () {
-            $('#details').removeClass('active');
-        });
+            $('#itemLists li').off('click').on('click', function () {
+                $('#items').addClass('active');
+            });
+
+            $('#backInbox').off('click').on('click', function () {
+                $('#items').removeClass('active');
+            });
+
+            $('#productPanel .productCard').off('click').on('click', function () {
+                $('#details').addClass('active');
+            });
+
+            $('#backItem').off('click').on('click', function () {
+                $('#details').removeClass('active');
+            });
+        }
     }
 
+    $(document).ready(function () {
+        adjustLayout();
+        dragableSec();
+    });
+
+    $(window).resize(function () {
+        adjustLayout();
+        dragableSec();
+    });
 
     $('.toggler').click(function () {
         $('#lists').toggleClass('hide');
         $('#items').toggleClass('hide');
         $('#details').toggleClass('widthFull');
-    })
+    });
 
-    if (screenWidth >= 991) {
-        $("#lists, #items").resizable({
-            handles: "e",
-            resize: function (event, ui) {
-                console.log("Resized", ui.size.width, ui.size.height);
-            }
-        });
+    function dragableSec() {
+        let screenWidth = $(window).width();
+
+        if (screenWidth >= 991) {
+            $("#lists, #items").resizable({
+                handles: "e",
+                resize: function (event, ui) {
+                    // console.log("Resized", ui.size.width, ui.size.height);
+                }
+            });
+        }
     }
 }(jQuery))
